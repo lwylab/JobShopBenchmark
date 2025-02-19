6bffe03cacde99511c237b87272b0fca2e2da849
梁文钰 <wenyu.liang@haiziwang.com>
2025-02-20 00:24:00 +0800

    Mk10，调度规则这么逆天？
    INFO:root:Makespan: 239
    Mk10——调度规则dispatching_rules——最小化最大完工时间——makespan:239——耗时:0.20866799354553223
    
    Signed-off-by: 梁文钰 <wenyu.liang@haiziwang.com>

---
+++ test_dispatching_rules.py
@@ -0,0 +1,38 @@
+import re
+import time
+
+from solution_methods.dispatching_rules.run_dispatching_rules import run_dispatching_rules
+from solution_methods.helper_functions import load_job_shop_env, load_parameters
+from visualization import gantt_chart
+
+if __name__ == '__main__':
+    # freeze_support() # 关闭之后，貌似也能正常工作
+
+    # 定义问题实例，也可以从配置文件中加载
+    # problem_instance = "/fjsp/fattahi/MFJS10.fjs"
+    problem_instance = "/fjsp/brandimarte/Mk10.fjs"
+
+    # 加载作业车间环境
+    jobShopEnv = load_job_shop_env(problem_instance)
+
+    # 对初始作业车间环境绘制甘特图没有意义
+    # plot_gantt_chart(jobShopEnv)
+
+    # 加载CP_SAT配置
+    parameters = load_parameters("configs/dispatching_rules.toml")
+
+    # 从此刻开始计时
+    start_time = time.time()
+
+    makespan, jobShopEnv = run_dispatching_rules(jobShopEnv, **parameters)
+
+    # 使用正则表达式分割字符串
+    instance = re.split(r'[/\.]', jobShopEnv.instance_name)
+
+    # 检查是否有足够的部分，并处理边界条件
+    title = instance[3] if len(instance) > 3 else "N/A"
+
+    print(
+        f"{title}——调度规则dispatching_rules——最小化最大完工时间——makespan:{makespan}——耗时:{time.time() - start_time}")
+
+    gantt_chart.plot(jobShopEnv).show();
