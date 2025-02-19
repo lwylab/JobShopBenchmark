42b234b645800ea7275faa9d69e76b6c3c223d0d
梁文钰 <wenyu.liang@haiziwang.com>
2025-02-20 00:55:22 +0800

    cp_sat
    Horizon = 3255
    Solution 0, time = 0.568294 s, objective = 652
    Solution 1, time = 0.587078 s, objective = 549
    Solution 2, time = 0.660102 s, objective = 538
    Solution 3, time = 0.677119 s, objective = 505
    Solution 4, time = 0.740667 s, objective = 373
    Solution 5, time = 1.178471 s, objective = 365
    Solution 6, time = 1.439734 s, objective = 357
    Solution 7, time = 1.782851 s, objective = 356
    Solution 8, time = 1.910761 s, objective = 338
    Solution 9, time = 2.339612 s, objective = 337
    Solution 10, time = 2.617146 s, objective = 336
    Solution 11, time = 2.715223 s, objective = 334
    Solution 12, time = 3.390484 s, objective = 333
    Solution 13, time = 3.504014 s, objective = 332
    Solution 14, time = 3.593815 s, objective = 331
    Solution 15, time = 3.779367 s, objective = 330
    Solution 16, time = 3.914248 s, objective = 329
    Solution 17, time = 4.257020 s, objective = 328
    Solution 18, time = 4.504768 s, objective = 327
    Solution 19, time = 4.574555 s, objective = 298
    Solution 20, time = 5.544119 s, objective = 297
    Solution 21, time = 5.671435 s, objective = 295
    Solution 22, time = 6.010286 s, objective = 290
    Solution 23, time = 6.594304 s, objective = 288
    Solution 24, time = 7.139700 s, objective = 284
    Solution 25, time = 7.770180 s, objective = 273
    Solution 26, time = 9.623973 s, objective = 270
    Solution 27, time = 9.703768 s, objective = 268
    Solution 28, time = 10.718088 s, objective = 267
    Solution 29, time = 11.825069 s, objective = 259
    Solution 30, time = 17.012449 s, objective = 255
    Solution 31, time = 18.012290 s, objective = 249
    Solution 32, time = 18.103041 s, objective = 248
    Solution 33, time = 18.903388 s, objective = 246
    Solution 34, time = 25.273574 s, objective = 238
    Solution 35, time = 28.620371 s, objective = 233
    Solution 36, time = 40.762870 s, objective = 232
    Solution 37, time = 43.448553 s, objective = 231
    Solution 38, time = 48.030745 s, objective = 229
    Solution 39, time = 53.899910 s, objective = 222
    Solution 40, time = 64.164692 s, objective = 221
    Solution 41, time = 83.354306 s, objective = 220
    Solution 42, time = 99.351861 s, objective = 219
    Solution 43, time = 117.911271 s, objective = 218
    Solution 44, time = 127.121562 s, objective = 217
    Solution 45, time = 141.090103 s, objective = 216
    Solution 46, time = 146.514989 s, objective = 215
    Solution 47, time = 334.457642 s, objective = 214
    Solution 48, time = 362.107863 s, objective = 213
    Solution 49, time = 392.211083 s, objective = 212
    Solution 50, time = 401.701857 s, objective = 211
    Solution 51, time = 404.699851 s, objective = 210
    Solution 52, time = 410.711236 s, objective = 209
    Solution 53, time = 662.945946 s, objective = 208
    
    Signed-off-by: 梁文钰 <wenyu.liang@haiziwang.com>

--- test_CP_SAT.py
+++ test_CP_SAT.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     # 检查是否有足够的部分，并处理边界条件
     title = instance[3] if len(instance) > 3 else "N/A"
 
-    # 使用 f-string 提高可读性和性能
+    # 使用 f-string 提高可读性和性能.
     # print(f"{title}——CP_SAT——最小化最大完工时间——makespan:{makespan}"
     #
     print(f"{title}，CP-SAT，最小化最大完工时间，makespan:{makespan}，耗时:{time.time() - start_time}")
